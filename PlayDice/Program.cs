30761a34b3dc7986815ec1abac490b222f82f3c4
pedro <p.martims120@gmail.com>
2023-10-11 15:08:08 -0300

    Adicionar arquivos de projeto.

---
+++ PlayDice/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PlayDice
+{
+    class project
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nome do primeiro jogador: ");
+            string name1 = Console.ReadLine();
+
+            Console.Write("Nome do segundo jogador: ");
+             string name2 = Console.ReadLine();
+
+            if (name1 != name2)
+            {
+                int n = 3;
+                int[] dados = new int[] {1,2,3,4,5,6};
+                for (int i = 0; i < n; i++)
+                {
+                    Console.WriteLine($"Rodada #{i}");
+
+                }
+            }
+
+        }
+    }
+}
