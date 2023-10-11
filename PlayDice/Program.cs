97b78000915b86daa6a56a5ec8ffe90a1a845505
pedro <p.martims120@gmail.com>
2023-10-11 15:35:16 -0300

    PlayDice

--- PlayDice/Program.cs
+++ PlayDice/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel.Design;
+using System.Diagnostics.CodeAnalysis;
 
 namespace PlayDice
 {
@@ -10,18 +12,41 @@ namespace PlayDice
             string name1 = Console.ReadLine();
 
             Console.Write("Nome do segundo jogador: ");
-             string name2 = Console.ReadLine();
+            string name2 = Console.ReadLine();
+            int n = 3;
+            int sum1 = 0;
+            int sum2 = 0;
 
-            if (name1 != name2)
+            Random random = new Random();
+            for (int i = 0; i < n; i++)
             {
-                int n = 3;
-                int[] dados = new int[] {1,2,3,4,5,6};
-                for (int i = 0; i < n; i++)
-                {
-                    Console.WriteLine($"Rodada #{i}");
+                Console.WriteLine($"Rodada #{i}");
+                int result1 = random.Next(1, 7);
+                Console.WriteLine($"{name1} tirou {result1}");
+                sum1 += result1;
 
-                }
+                int result2 = random.Next(1, 7);
+                Console.WriteLine($"{name2} tirou {result2}");
+                sum2 += result2;
             }
+            
+            Console.WriteLine($"Total de {name1}: {sum1}");
+            Console.WriteLine($"Total de {name2}: {sum2}");
+
+            if (sum1 > sum2)
+            {
+                Console.WriteLine($"{name1} ganhou");
+            }
+            else if (sum1 < sum2)
+            {
+                Console.WriteLine($"{name2} ganhou");
+            }
+            else
+            {
+                Console.WriteLine("EMPATE");
+            }
+
+
 
         }
     }
